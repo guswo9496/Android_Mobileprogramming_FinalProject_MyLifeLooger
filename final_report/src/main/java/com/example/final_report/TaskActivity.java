454216fd633aceb1eca15a909ed66b779bb1946e
guswo9496 <guswo9496@naver.com>
2016-11-29 11:29:43 +0900

    Layout3(task spinner)

--- final_report/src/main/java/com/example/final_report/TaskActivity.java
+++ final_report/src/main/java/com/example/final_report/TaskActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,6 +19,22 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+      final TextView tv =(TextView)findViewById(R.id.tasktextview1);
+        Spinner spinner =(Spinner)findViewById(R.id.taskspinner1);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int TaskIndex, long id) {
+                tv.setText("TaskInDex : "+TaskIndex+parent.getItemAtPosition(TaskIndex));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
         findViewById(R.id.taskbottombutton1).setOnClickListener(t1ClickListener);
         findViewById(R.id.taskbottombutton2).setOnClickListener(t1ClickListener);
